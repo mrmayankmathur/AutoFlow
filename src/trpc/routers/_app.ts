c07c4fdd7ed90a1dd28bb4e02d3820c7b67c6e87
mrmayankmathur <mrmayankmathur@gmail.com>
2025-12-21 03:26:07 +0530

    Implement workflow management with a new database table, Inngest background processing, and a dedicated dashboard UI.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,21 @@
+import { inngest } from "@/inngest/client";
 import { protectedProcedure, createTRPCRouter } from "../init";
 import { prisma } from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     console.log({ userId: ctx.auth.user.id });
 
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "mrmayankmathur@gmail.com",
       },
     });
+    return { success: true, message: "Job queued!" };
   }),
 });
 // export type definition of API
