8d0c2f2b937ccaf7b0ed0c39ee7ef8107bfdd11d
mrmayankmathur <mrmayankmathur@gmail.com>
2025-12-21 16:15:43 +0530

    AI Providers and streamline Prisma client setup.

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,23 +1,62 @@
-import { prisma } from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createGateway, generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    // fetching data from an API
-    await step.sleep("fetching-data", "5s");
-    // transcribing data from an API
-    await step.sleep("transcribing-data", "5s");
-    // sending it to AI
-    await step.sleep("sending-to-ai", "5s");
+/* ---------------- Providers ---------------- */
 
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "workflow-from-inngest",
-        },
-      });
-    });
+const google = createGoogleGenerativeAI();
+
+export const openai = createOpenAI({
+  apiKey: process.env.GITHUB_TOKEN!,
+  baseURL: "https://models.github.ai/inference",
+});
+
+const anthropic = createAnthropic();
+
+/* ---------------- Inngest Function ---------------- */
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
+  async ({ step }) => {
+    await step.sleep("pretend", "5s");
+
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-3-flash-preview"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "github-openai",
+      generateText,
+      {
+        model: openai.chat("gpt-4.1"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   }
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,16 @@
 import { inngest } from "@/inngest/client";
 import { protectedProcedure, createTRPCRouter } from "../init";
 import { prisma } from "@/lib/db";
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
 
 export const appRouter = createTRPCRouter({
+  testAI: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+    return { success: true, message: "Job queued!" };
+  }),
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     console.log({ userId: ctx.auth.user.id });
 
