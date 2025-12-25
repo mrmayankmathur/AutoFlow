be0d8b87084ed4a8507d09ed6a9455b72b2bf29d
mrmayankmathur <mrmayankmathur@gmail.com>
2025-12-26 02:26:13 +0530

    Implemented full CRUD functionality and UI for workflows.

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,35 +1,8 @@
-import { inngest } from "@/inngest/client";
-import {
-  protectedProcedure,
-  createTRPCRouter,
-  premiumProcedure,
-} from "../init";
-import { prisma } from "@/lib/db";
-import { generateText } from "ai";
-import { google } from "@ai-sdk/google";
-import { TRPCError } from "@trpc/server";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAI: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return { success: true, message: "Job queued!" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    console.log({ userId: ctx.auth.user.id });
-
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "mrmayankmathur@gmail.com",
-      },
-    });
-    return { success: true, message: "Job queued!" };
-  }),
+  workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
